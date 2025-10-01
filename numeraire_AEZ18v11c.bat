25bde2a7ef79eddf76501aebc0bbf7c07e99b45f
nvilloria <nvilloria@gmail.com>
2025-10-01 12:05:32 -0500

    Miscellaneous updates

---
+++ numeraire_AEZ18v11c.bat
@@ -0,0 +1,3 @@
+gtapaez3 -cmf numeraire_AEZ18v11c.cmf
+sltoht -map=decomp.map ./AEZ18v11c/numeraire.sl4 ./AEZ18v11c/numeraire.sol
+rem decomp -cmf welfare_decomp.cmf
---
+++ numeraire_BIOME14v11c.bat
@@ -0,0 +1,3 @@
+gtapaez3 -cmf numeraire_BIOME14v11c.cmf
+sltoht -map=decomp.map ./BIOME14v11c/numeraire.sl4 ./BIOME14v11c/numeraire.sol
+rem decomp -cmf welfare_decomp.cmf
